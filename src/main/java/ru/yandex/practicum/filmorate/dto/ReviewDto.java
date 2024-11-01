06c5faf0cd04fe871d3aef74740141286f084df6
Altana Batueva <altana.batueva0@gmail.com>
2024-11-01 23:43:46 +0100

    useful can be negative

--- src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
+++ src/main/java/ru/yandex/practicum/filmorate/dto/ReviewDto.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.dto;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Data;
 
@@ -22,7 +21,6 @@ public class ReviewDto {
     boolean isPositive;
 
     @NotNull
-    @Positive
     Integer useful;
 
     String content;
